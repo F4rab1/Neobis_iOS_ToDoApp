d25d740ebde922bbaf99a2812f3443ed491d6e25
F4rab1 <issafarabi2022@gmail.com>
2023-04-25 23:27:09 +0600

    Initial Commit

---
+++ ToDoList_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList_ios
+//
+//  Created by Фараби Иса on 25.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
